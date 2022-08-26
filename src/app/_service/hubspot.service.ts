0ec2e8327f4a4a588a384af935a5a8b8522be1a7
R. Geelen <roel-geelen@hotmail.com>
2022-08-26 08:57:21 +0200

    sign multiple quotes

--- src/app/_service/hubspot.service.ts
+++ src/app/_service/hubspot.service.ts
@@ -15,16 +15,22 @@ export class HubspotService {
     return this.http.get<any>(`${environment.apiUrl}deal/${dealID}`)
   }
 
-  uploadSignature(dealID: string | null, name: string, file: string): Observable<HttpEvent<{}>> {
+  uploadSignature(dealID: string | null, configID: string | null, name: string, file: string): Observable<HttpEvent<{}>> {
     const formdata: FormData = new FormData();
     formdata.append('file', file);
     // @ts-ignore
     formdata.append('dealid', dealID);
+    // @ts-ignore
+    formdata.append('quoteId', configID);
     formdata.append('name', name);
     const req = new HttpRequest('POST', `${environment.apiUrl}upload/sign`, formdata, {
       reportProgress: true,
       responseType: 'text'
     });
     return this.http.request(req);
+  }
+
+  getImage(id: string | null) {
+    return this.http.get<any>('https://25493451.fs1.hubspotusercontent-eu1.net/hubfs/25493451/Signatures/handtekening-' + id + '.png?timeStamp='+Date.now());
   }
 }
--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -17,6 +17,7 @@ export class SignComponent implements OnInit {
   progress: { percentage: number } = {percentage: 0};
   signed = false;
   dealID: string | null = '';
+  configID: string | null = '';
   name: string = '';
   error: string = '';
   required: string = '';
@@ -40,18 +41,35 @@ export class SignComponent implements OnInit {
         this.error = 'Geen deal id aanwezig';
         this.loading = false;
       }
-      this.dealID = queryParams.get('id');
-      this.hubspotService.getDealByID(this.dealID).subscribe(r => {
-        this.deal = r;
-        this.error = '';
-        this.loading = false;
-        if (this.deal?.properties?.signature) {
-          this.signatureImg = 'https://25493451.fs1.hubspotusercontent-eu1.net/hubfs/25493451/Signatures/handtekening-' + this.dealID + '.png';
+      this.route.queryParams.subscribe(params => {
+        if (params['deal']) {
+          this.dealID = params['deal'];
+          this.configID = queryParams.get('id');
+        } else {
+          this.dealID = queryParams.get('id');
+          this.configID = queryParams.get('id');
         }
-      }, error => {
-        this.error = 'Deal ' + this.dealID + ' niet gevonden';
-        this.loading = false;
       })
+
+      this.hubspotService.getImage(this.configID).subscribe(null, error1 => {
+        console.log(error1)
+        if (error1.status == 200) {
+          this.signatureImg = 'https://25493451.fs1.hubspotusercontent-eu1.net/hubfs/25493451/Signatures/handtekening-' + this.configID + '.png?timeStamp='+Date.now();
+        }
+        this.loading = false;
+      });
+
+      // this.hubspotService.getDealByID(this.dealID).subscribe(r => {
+      //   this.deal = r;
+      //   this.error = '';
+      //   this.loading = false;
+      //   if (this.deal?.properties?.signature) {
+      //     this.signatureImg = 'https://25493451.fs1.hubspotusercontent-eu1.net/hubfs/25493451/Signatures/handtekening-' + this.configID + '.png';
+      //   }
+      // }, error => {
+      //   this.error = 'Deal ' + this.dealID + ' niet gevonden';
+      //   this.loading = false;
+      // })
     });
   }
 
@@ -69,12 +87,12 @@ export class SignComponent implements OnInit {
     this.signed = true;
     // @ts-ignore
     const base64Data = this.signaturePad.toDataURL();
-    this.hubspotService.uploadSignature(this.dealID, this.name, base64Data).subscribe(r => {
+    this.hubspotService.uploadSignature(this.dealID, this.configID, this.name, base64Data).subscribe(r => {
       if (r.type === HttpEventType.UploadProgress) {
         // @ts-ignore
         this.progress.percentage = Math.round(100 * r.loaded / r.total);
       } else if (r instanceof HttpResponse) {
-        this.signatureImg = 'https://25493451.fs1.hubspotusercontent-eu1.net/hubfs/25493451/Signatures/handtekening-' + this.dealID + '.png';
+        this.signatureImg = 'https://25493451.fs1.hubspotusercontent-eu1.net/hubfs/25493451/Signatures/handtekening-' + this.configID + '.png?timeStamp='+Date.now();
         this.loading = false;
       }
     })
